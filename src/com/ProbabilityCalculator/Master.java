2916980ea4f94a309bc12e623d85588ac4eade76
Connor Adams <cleeadacon909@gmail.com>
2022-07-17 16:49:40 -0700

    Create Master.java

---
+++ src/com/ProbabilityCalculator/Master.java
@@ -0,0 +1,14 @@
+package com.ProbabilityCalculator;
+
+import com.sun.org.apache.xpath.internal.operations.Variable;
+
+public class Master(String col) {
+
+    String color = col;
+
+    public static void main(String[] args) {
+        Master hat = new Master('blue');
+
+        System.out.println(hat);
+    }
+}
